77c82a4a253beb554d45d909a5b17a98cc5ca5d2
David Polania <davidpolaniaac@users.noreply.github.com>
2019-08-21 12:16:14 -0500

    update package and delete task utils

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,15 @@
-import * as common from 'azure-devops-task-utils';
 import * as nodeApi from 'azure-devops-node-api';
 import * as GitApi from 'azure-devops-node-api/GitApi';
 import * as RemoteConfig from './remoteConfig';
 import * as IRemoteConfig from './interfaces/RemoteConfig';
 
 export async function getRemoteConfigApi(
+    webApi: nodeApi.WebApi,
+    project: string,
     repositoryId: string,
     pathConfig: string,
-    commonApi = common,
 ): Promise<IRemoteConfig.RemoteConfigApi> {
-    const webApi: nodeApi.WebApi = await commonApi.getWebApi();
     const gitApi: GitApi.IGitApi = await webApi.getGitApi();
-    const project: string = common.getProject();
     const remoteConfig: RemoteConfig.RemoteConfigBase = new RemoteConfig.RemoteConfigBase(
         project,
         repositoryId,
