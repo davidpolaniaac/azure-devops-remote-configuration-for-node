8e5ad0a1c669475269caead859fe05f52dccbcb2
David Polania <davidpolaniaac@users.noreply.github.com>
2023-12-05 22:57:42 -0500

    export api class

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { IRemoteConfigApi, IRemoteConfigBase } from './interfaces/RemoteConfig';
 import { IGitApi } from 'azure-devops-node-api/GitApi';
 import { RemoteConfigBase } from './remoteConfig';
 import { WebApi } from 'azure-devops-node-api';
-
+export { RemoteConfigApi } from './remoteConfigApi';
 export { IRemoteConfigApi } from './interfaces/RemoteConfig';
 export async function getRemoteConfigApi(
     webApi: WebApi,
